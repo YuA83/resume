d61be799d353b8d4e4872ea03ba7bc90808cfbb7
유경아 <YuA@MacBookAir>
2025-09-02 09:57:10 +0900

    mod: profile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,29 @@
 import React from "react";
 
-const Header = ({
-                  isKorean,
-                  nameTitle,
-                  addressTitle
-}) => {
-
+const Header = ({ isKorean, nameTitle, addressTitle }) => {
   return (
-
     // Profile Section
     <div className="header">
       <div className="photo">
-        <img src={`${process.env.PUBLIC_URL}/assets/images/profile.jpg`} alt="프로필 사진"/>
+        <img
+          src={`${process.env.PUBLIC_URL}/assets/images/maru.jpg`}
+          alt="프로필 사진"
+        />
       </div>
       <div className="personal-info">
         <h1>{isKorean ? nameTitle.ko : nameTitle.en}</h1>
 
-        <br/>
-        <br/>
+        <br />
+        <br />
 
         <p>🎂 1998.08.03</p>
-        <p>{isKorean ? addressTitle.ko: addressTitle.en}</p>
-        <p><a href="tel:+821092588393">📞 +82 10-9258-8393</a></p>
-        <p><a href="mailto:yua._.83@icloud.com">📧 yua._.83@icloud.com</a></p>
+        <p>{isKorean ? addressTitle.ko : addressTitle.en}</p>
+        <p>
+          <a href="tel:+821092588393">📞 </a>
+        </p>
+        <p>
+          <a href="mailto:yua._.83@icloud.com">📧 yua._.83@icloud.com</a>
+        </p>
       </div>
     </div>
   );
